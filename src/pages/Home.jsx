ec40dfd5c4b2dabc54938331faae9ef44e2f5ddb
luizfrz <luizfranca2005@gmail.com>
2026-05-06 17:28:55 -0300

    feat: graphic in home point

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,36 @@
 import { useNavigate } from 'react-router-dom'
+import { Doughnut } from "react-chartjs-2";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
 import "../styles/Home.css"
+
+ChartJS.register(ArcElement, Tooltip, Legend);
 
 function Home({ records, setRecords }) {
   const navigate = useNavigate()
   const Clear = () => setRecords([])
+  
   const formatTime = (date) => date.toLocaleTimeString('pt-BR')
   const addRecord = (type) => setRecords(prev => [...prev, { type, time: new Date() }])
+
+  const entrada = records.filter(r => r.type?.toLowerCase() === "entrada").length
+  const pausa = records.filter(r => r.type?.toLowerCase() === "pausa").length
+  const saida = records.filter(r => r.type?.toLowerCase() === "saída").length
+
+
+  const chartData = {
+    labels: ["entrada", "pausa", "saida"],
+    datasets: [
+      {
+        data: [
+          entrada,
+          pausa,
+          saida
+        ],
+        backgroundColor: ["#12a712", "#c7aa08","#ca2557"],
+        borderWidth:1,
+      },
+    ],
+  };
 
   return (
     <div>
@@ -24,7 +49,8 @@ function Home({ records, setRecords }) {
       <div className='General'>
         <h1>Visão geral</h1>
         <div>
-          <h2>Grafico de Pontos</h2>
+          <h2> Pontos batido no dia </h2>
+          <Doughnut  data={chartData} />
         </div>
         <br/>
         <div>
@@ -35,15 +61,15 @@ function Home({ records, setRecords }) {
       <div className='Last'>
         <h1> Últimos registros</h1>
         {records.length > 0 ? (
-          <ul>
+          <ul >
             {records.slice(-5).map((record, index) => (
-              <li key={index}>
+              <li className='Records' key={index}> 
                 {record.type}- {formatTime(record.time)}
               </li>
             ))}
           </ul>
         ) : (
-          <p className='N-Register'>Nenhum registro ainda</p>
+          <p className='N-Records'>Nenhum registro ainda</p>
         )}
       </div>
       <div className="ClearAll"> 
